58e2b1fb499f05cc793e960d7ed03bd705c8ec2a
springpoet <ksksksksksmss@gmail.com>
2022-08-31 16:42:40 +0900

    220831 프로젝트 update

---
+++ 김성근_SQL/project_Table_edit.sql
@@ -0,0 +1,54 @@
+--drop table ticket;
+create table ticket(
+ticket_num varchar2(30) primary key,
+flight_num varchar2(30),
+name varchar2(30),
+gate number,
+seat varchar2(30),
+boarding number,
+depart_nation varchar2(30),
+arrive_nation varchar2(30));
+
+--drop table flight;
+create table flight(
+flight_num varchar2(30) primary key,
+dated date,
+gate number,
+departure_time number,
+depart_nation varchar2(30),
+arrive_nation varchar2(30),
+airport varchar2(30));
+
+--drop table passenger;
+create table passenger(
+passport varchar2(30) primary key,
+name varchar2(30),
+birth number,
+phone varchar2(30));
+
+--drop table depart_nation;
+create table depart_nation(
+code varchar2(30) primary key,
+name varchar2(30));
+
+
+--drop table arrive_nation;
+create table arrive_nation(
+code varchar2(30) primary key,
+name varchar2(30));
+
+
+--drop table airport;
+create table airport(
+code varchar2(30) primary key,
+name varchar2(30));
+
+alter table ticket add constraint fk_name foreign key(name) references passenger(passport); 
+alter table ticket add constraint fk_depnation foreign key(depart_nation) references depart_nation(code);
+alter table ticket add constraint fk_arrivenation foreign key(arrive_nation) references arrive_nation(code);
+
+alter table flight add constraint fk_departure foreign key(depart_nation) references depart_nation(code);
+alter table flight add constraint fk_arrived foreign key(arrive_nation) references arrive_nation(code);
+alter table flight add constraint fk_airport foreign key(airport) references airport(code);
+
+
